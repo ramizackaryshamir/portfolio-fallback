214be3e6a0bc685902952d5d933d4f0a4562de11
MeetYourCreator <rzshamir@icloud.com>
2024-06-06 10:42:03 -0400

    styling fixes

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -4,13 +4,11 @@ import '../../style.css';
 const Video = ({ src }) => {
   return (
     <>
-      {/* <section
-        className="video"
-      > */}
+
       <video autoPlay={true} muted={true} loop={true} playsInline={true} className="video">
         <source src={src} type="video/mp4" />
       </video>
-      {/* </section> */}
+
     </>
   );
 };
