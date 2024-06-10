e4ae67b36618b65f954f4fe5b9d0b24fffdc3027
MeetYourCreator <rzshamir@icloud.com>
2024-06-10 18:00:26 -0400

    clean up code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test( 'renders Discover Financial Services heading', () => {
+  render( <App /> );
+  const headingElement = screen.getByText( /Discover Financial Services/i );
+  expect( headingElement ).toBeInTheDocument();
+} );
