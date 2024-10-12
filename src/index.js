544827138f7ef2a01a70cb9a7641869c0319f7d8
RamiZackaryShamir <rami.zackary.shamir@icloud.com>
2024-10-12 10:23:37 -0400

    refactor React Router logic and delete App.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Home from './screens/Home/Home.jsx';
+import Contact from './screens/Contact/Contact.jsx';
+import Project from './components/Project/Project.jsx'
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/contact',
+    element: <Contact />
+  }
+])
+
+
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
